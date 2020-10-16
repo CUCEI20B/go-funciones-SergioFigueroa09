13a539ce2b8b0d77453a32ee83167787482dee27
unknown <sergio.figueroa7875@alumnos.udg.mx>
2020-10-16 16:15:18 -0500

    Burbuja v0.1

--- burbuja.go
+++ burbuja.go
@@ -1,7 +1,16 @@
 package main
 
 func Burbuja(s []int64)  {
-
+	var aux int64
+	for i:= len(s); i > 0; i--{
+		for j:=1; j < i; j++{
+			if s[j-1] > s[j]{
+				aux = s[j]
+				s[j] = s[j-1]
+				s[j-1] = aux
+			}
+		}
+	}
 }
 
 func main()  {
